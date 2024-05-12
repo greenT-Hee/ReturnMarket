c22d11acaa8aa6ac2a896a7e0ad16b5bda14662f
greenT-Hee <heehee970322@gmail.com>
2024-05-13 08:45:13 +0900

    global css setting

--- tennis-market/src/App.jsx
+++ tennis-market/src/App.jsx
@@ -2,10 +2,18 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainPage from './pages/mainPage';
 import LoginPage from './pages/loginPage';
+import reset from 'styled-reset';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  /* other styles */
+`
 
 function App() {
   return (
     <BrowserRouter>
+    <GlobalStyle/>
     <Routes>
       <Route path="/" element={<MainPage />}></Route>
       <Route path="/login" element={<LoginPage />}></Route>
