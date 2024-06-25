398016d2dfdbccf81a480b91feb2c6448cd7b2c2
greenT-Hee <heehee970322@gmail.com>
2024-06-25 15:18:38 +0900

    배포 설정 수정

--- tennis-market/vite.config.js
+++ tennis-market/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { redirect } from 'react-router-dom'
 
 // https://vitejs.dev/config/
 export default defineConfig({
